77ab5c42e79403c8458090506dcb133241705012
Stéphane Francel <contact@avine.io>
2017-08-07 14:41:17 +0200

    Use app-todo-item as tag selector - Replace <table> with Bootstrap grid

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: '[app-todo-item]',
+  selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
