f0d6694c2f467a520b13e4215324aa9f7a7caad8
Stéphane Francel <contact@avine.io>
2017-08-11 12:09:24 +0200

    Improve build.prod.js (deploy.js renamed)

---
+++ src/scripts/build.prod.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const cp = require('child_process');
+const fs = require('fs');
+
+const argv = yargs.argv;
+const exec = command => cp.execSync(command, {stdio: 'inherit'});
+
+// (option: --bh) Base href
+const bh = argv['bh'] || '/';
+
+// (option: --langs) List of languages separated by `,``
+// Notice: the first language is the one used in the app source code
+// (which don't have "messages.<lang>.xlf" defined)
+const langs = argv['langs'] ? argv['langs'].split(',') : [];
+
+// (option: --dl) Default language to use in apache config when redirecting bad url
+const defaultLang = langs.length ? (argv['dl'] || langs[0]) : null;
+
+// Execute build command according to "<lang>" and "messages.<lang>.xlf" availibility
+const execBuildCmd = (lang, hasXlf) => {
+  let cmd;
+  if (!lang) {
+    cmd = `ng build --output-path=dist/ --aot -prod --bh ${bh}`;
+  } else {
+    const xlfOptions = hasXlf ? '--i18n-file=src/i18n/messages.$lang.xlf --i18n-format=xlf' : '';
+    cmd = `lang=${lang}; ng build --output-path=dist/$lang --aot -prod --bh ${bh}$lang/ --locale=$lang ${xlfOptions}`;
+  }
+  console.log(`\nBuilding "dist/${lang || ''}"...`);
+  console.log(`${cmd}\n`);
+  exec(cmd);
+};
+
+// Start building...
+
+console.log('### Building App for production ###');
+console.log(`\nUsing baseHref: "${bh}"`);
+
+if (!langs.length) {
+  // Build app
+  execBuildCmd();
+} else {
+  console.log(`\nDefault language (for apache config): "${defaultLang}"`);
+
+  // Build app for each language
+  langs.forEach((lang, index) => execBuildCmd(lang, index !== 0));
+
+  // Build apache config
+  let htaccess = fs.readFileSync('src/apache/.htaccess', 'utf8');
+  htaccess =
+    htaccess
+      .replace(/{{BASE_HREF}}/g, bh)
+      .replace(/{{LANGS}}/g, `(${langs.join('|')})`)
+      .replace(/{{DEFAULT_LANG}}/g, defaultLang);
+
+  let index = fs.readFileSync('src/apache/index.html', 'utf8');
+  index = index.replace(/{{DEFAULT_LANG}}/g, defaultLang);
+
+  console.log('\nBuilding apache config: "dist/.htaccess" and "dist/index.html"');
+  fs.writeFileSync('dist/.htaccess', htaccess, 'utf8');
+  fs.writeFileSync('dist/index.html', index, 'utf8');
+}
